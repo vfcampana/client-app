364e339c2a8b6c91297f773313970f48c39f6e54
Vinicius <83793571+viniciusxv27@users.noreply.github.com>
2025-03-06 20:40:00 -0300

    atualizar lote

--- blockapi/api/resources/lote.py
+++ blockapi/api/resources/lote.py
@@ -68,8 +68,73 @@ class LoteGet(Resource):
         return response
 
 class LoteAtualiza(Resource):
-    def put(self):
-        return jsonify({'lote': 'lote'})
+    def put(self, id):
+
+        id_usuario = verificar_jwt()
+
+        if id_usuario['code'] != 200:
+            response = jsonify({"message": id_usuario['message']})
+            response.status_code = id_usuario['code']
+            return response
+        
+        data = request.get_json()
+
+        lote = session.query(Lote).filter(Lote.id == id).filter(Lote.id_usuario == id_usuario['message']).first()
+
+        if not lote:
+            response = jsonify({"message": "Lote não encontrado"})
+            response.status_code = 404
+            return response
+        
+        lote.nome = data.get('nome', lote.nome)
+        lote.preco = data.get('preco', lote.preco)
+        lote.observacoes = data.get('observacoes', lote.observacoes)
+        lote.status = data.get('status', lote.status)
+
+        if data.get('blocos'):
+            lote_bloco = session.query(LoteBlocos).filter(LoteBlocos.id_lote == id).all()
+            
+            for lote_bloco in lote_bloco:
+                session.delete(lote_bloco)
+
+            for bloco in data.get('blocos'):
+                bloco = session.query(Bloco).filter(Bloco.id == bloco).first()
+                if not bloco:
+                    response = jsonify({"message": "Bloco não encontrado"})
+                    response.status_code = 404
+                    return response
+                if bloco.id_usuario != id_usuario['message']:
+                    response = jsonify({"message": "Bloco não pertence ao usuário"})	
+                    response.status_code = 404
+                    return response
+                
+                lote_blocos = LoteBlocos(
+                    id_bloco=bloco.id,
+                    id_lote=lote.id
+                )
+                session.add(lote_blocos)
+
+            session.commit()
+            session.close()
+
+            response = jsonify({"message": "Lote atualizado com sucesso"})
+            response.status_code = 200
+            return response
+        
+                
+
+                
+
+                
+
+        session.commit()
+        session.close()
+
+        response = jsonify({"message": "Lote atualizado com sucesso"})
+        response.status_code = 200
+        return response
+        
+
 
 class LoteCadastro(Resource):
     def post(self):
